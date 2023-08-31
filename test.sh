b38683fe29586c282a4102d3e92496d9c71f2d4a
vladislav-bakanov <rewerty52@ya.ru>
2023-08-31 19:08:49 +0300

    Add tests to project

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Starting script execution..."
+
+echo "Testing all files with mypy"
+poetry run mypy --exclude 'flycheck_*' ./
+
+echo "Running pytest..."
+poetry run pytest --cov=partition_registry ./tests $@
+
+echo "Running linter"
+poetry run pylint --rcfile=setup.cfg --exit-zero ./partition_registry
+
+echo "Script execution completed successfully!"
