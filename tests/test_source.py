90e10367278146bbc76b87ecc86f3bebafcf1777
vladislav-bakanov <rewerty52@ya.ru>
2023-09-04 16:00:14 +0300

    Update source tests

--- tests/test_source.py
+++ tests/test_source.py
@@ -1,55 +1,29 @@
 import hypothesis.strategies as st
 from hypothesis import given
 
+from typing import Union
+
 from partition_registry.data.source import BigQuerySource
-from partition_registry.data.source import PentahoSource
-from partition_registry.data.provider import Provider
+from partition_registry.data.source import PostgreSQLSource
+from partition_registry.data.source import AirflowDAGSource
 
-from partition_registry.data.partition_strategy import PartitionStrategy
-
-from tests.arbitrary.source import arb_broken_bigquery_name
-from tests.arbitrary.source import arb_broken_pentaho_name
-from tests.arbitrary.partition_strategy import arb_partitioned
-from tests.arbitrary.partition_strategy import arb_not_partitioned
-
-from tests.arbitrary.provider import arb_provider
+from tests.arbitrary.source import arb_incorrect_bigquery_source
+from tests.arbitrary.source import arb_incorrect_postgresql_source
 
 from partition_registry.data.exceptions import IncorrectSourceNameError
 
 
 @given(
-    source_name=arb_broken_bigquery_name,
-    partition_strategy=st.sampled_from([arb_partitioned, arb_not_partitioned]),
-    provider=arb_provider,
+    source=st.one_of([arb_incorrect_bigquery_source, arb_incorrect_postgresql_source]),
 )
-def test_bigquery_source_with_broken_name(
-    source_name: str,
-    partition_strategy: PartitionStrategy,
-    provider: Provider
+def test_1(
+    source: Union[BigQuerySource, PostgreSQLSource, AirflowDAGSource]
 ) -> None:
-    """Test bigquery source with bad Source name"""
+    """
+    Test sources with incorrect name validation
+    """
     try:
-        BigQuerySource(source_name, partition_strategy=partition_strategy, provider=provider)
+        source.validate()
+        assert False, f"Source \"{str(source.source_name)}\" has been successfully initialized with incorrect parameters..."
     except IncorrectSourceNameError:
-        assert True
-    else:
-        assert False
-
-
-@given(
-    source_name=arb_broken_pentaho_name,
-    partition_strategy=st.sampled_from([arb_partitioned, arb_not_partitioned]),
-    provider=arb_provider,
-)
-def test_pentaho_source_with_broken_name(
-    source_name: str,
-    partition_strategy: PartitionStrategy,
-    provider: Provider
-) -> None:
-    """Test Pentaho source with Bad name"""
-    try:
-        PentahoSource(source_name, partition_strategy=partition_strategy, provider=provider)
-    except IncorrectSourceNameError:
-        assert True
-    else:
-        assert False
+        pass
