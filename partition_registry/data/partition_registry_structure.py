feed7a655b3e5aa6163c04b4558985669a4c9a36
vladislav-bakanov <rewerty52@ya.ru>
2023-09-04 13:39:42 +0300

    Update source algorithm

--- partition_registry/data/partition_registry_structure.py
+++ partition_registry/data/partition_registry_structure.py
@@ -1,59 +1,81 @@
-from typing import Tuple
+import collections
+
+from typing import Union
 from typing import Dict
 from typing import List
-import datetime as dt
 
 from partition_registry.data.partition_registry_event import PartitionRegistryEvent
 from partition_registry.data.partition import Partition
-from partition_registry.data.source import Source
-from partition_registry.data.partition import PartitionSize
-from partition_registry.data.event_state import EventState
+from partition_registry.data.provider import Provider
+
+from partition_registry.data.source import BigQuerySource
+from partition_registry.data.source import PostgreSQLSource
+from partition_registry.data.source import AirflowDAGSource
 
 
 class PartitionRegistryStructure:
     def __init__(
         self,
-        # source: Source,
         events: List[PartitionRegistryEvent]
     ) -> None:
-        # self.source = source
-        self.events = self.optimize_event_stream(events)
+        self.formatted_events = self.format_event_stream(events)
 
-    def optimize_event_stream(
+    def format_event_stream(
         self,
         events: List[PartitionRegistryEvent]
-    ) -> Dict[PartitionSize, List[PartitionRegistryEvent]]:
+    ) -> Dict[
+        Union[BigQuerySource, PostgreSQLSource, AirflowDAGSource],
+        Dict[
+            Provider,
+            List[PartitionRegistryEvent]
+        ]
+    ]:
         """
         Event stream optimizer allows left only last actions for the spesified interval
         Optimizer complexity is O(n)
-        
+
         Args:
             events (List[PartitionRegistryEvent]): _description_
 
         Returns:
             List[PartitionRegistryEvent]: _description_
         """
-        optimized_event_stream: Dict[PartitionSize, Dict[Partition, PartitionRegistryEvent]] = {}
+        formatted_event_stream: Dict[
+            Union[BigQuerySource, PostgreSQLSource, AirflowDAGSource],
+            Dict[
+                Provider,
+                Dict[Partition, PartitionRegistryEvent]
+            ]
+        ] = {}
 
         for event in events:
             event.validate()
-            # if event.partition.source != self.source:
-            #     continue
 
-            partition_size = PartitionSize(event.partition.size_in_sec)
-            if partition_size not in optimized_event_stream:
-                optimized_event_stream[partition_size] = {}
-            
+            if event.source not in formatted_event_stream:
+                formatted_event_stream[event.source] = {}
+
+            if event.provider not in formatted_event_stream[event.source]:
+                formatted_event_stream[event.source][event.provider] = {}
+
             if (
-                event.partition not in optimized_event_stream[partition_size]
+                event.partition not in formatted_event_stream[event.source][event.provider]
                 or
-                event.created_date > optimized_event_stream[partition_size][event.partition].created_date
+                event.created_date > formatted_event_stream[event.source][event.provider][event.partition].created_date
             ):
-                optimized_event_stream[partition_size][event.partition] = event
+                formatted_event_stream[event.source][event.provider][event.partition] = event
 
-        return {partition_size: list(v.values()) for partition_size, v in optimized_event_stream.items()}
+        return {
+            source: {provider: list(partition_info.values())}
+            for source, provider_info in formatted_event_stream.items()
+            for provider, partition_info in provider_info.items()
+        }
 
-    def is_partition_ready(self, desired_partition: Partition) -> bool:
+    def is_partition_ready(
+        self,
+        source: Union[BigQuerySource, PostgreSQLSource, AirflowDAGSource],
+        providers: List[Provider],
+        desired_partition: Partition
+    ) -> bool:
         """Is desired partition ready based on known events.
 
         Args:
@@ -62,19 +84,31 @@ class PartitionRegistryStructure:
         Returns:
             bool: is partition ready
         """
+
         # In case when we don't know nothing about the partition
         # we can't say that it's ready. Hence we should say that it's not ready
-        if not self.events:
+        if source not in self.formatted_events:
             return False
 
-        for _, events in self.events.items():
+        source_events = self.formatted_events[source]
+
+        # Case when we know there is provider, but we don't have events for it
+        if not collections.Counter(providers) == collections.Counter(list(source_events.keys())):
+            return False
+
+        for provider in providers:
+            provider_events = source_events[provider]
+            # In case we don't have provider in the list, but there are no events for the provider
+            if not provider_events:
+                return False
+
             total_seconds: float = 0
-            for event in events:
+            for event in provider_events:
                 if event.partition.startpoint > desired_partition.endpoint:
-                    pass
+                    continue
 
                 if event.partition.endpoint < desired_partition.startpoint:
-                    pass
+                    continue
 
                 if not event.is_ready:
                     return False
